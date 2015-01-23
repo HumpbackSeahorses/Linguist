9128c6683f1755f9285bb235c79f87d2eb338a2a
minocys <harich.leung@gmail.com>
2015-01-22 20:15:33 -0500

    finished backbone chat

--- public/client/app.js
+++ public/client/app.js
@@ -1,16 +1,7 @@
+var socket = io();
 
-// var socket = io();
-// $('form').submit(function(){
-//   socket.emit('chat message', $('#m').val());
-//   $('#m').val('');
-//   return false;
-// });
-// socket.on('chat message', function(msg){
-//   $('#messages').append($('<li>').text(msg));
-// });
-
-var message = Backbone.Model.extend({
-
+//setup backbone message model
+var Message = Backbone.Model.extend({
   defaults:{
     username:'',
     text: '',
@@ -19,57 +10,82 @@ var message = Backbone.Model.extend({
   }
 });
 
-var messages = Backbone.Collection.extend({
+var Messages = Backbone.Collection.extend({
 
-  model: message,
+  model: Message,
 
-  //ajax get to our server
-  getMsg: function(){},
-
-  //ajax post to our server
-  postMsg: function(){
-
+  //instantiate new model
+  //add model to collection
+  addmsg: function(msg){
+    var something = new Message({text: msg});
+    this.add(something);
   }
 
-  //ajax post to translate server
+  //TODO: ajax post to translate server
   // translate: function(){}
 
 });
 
-var submitView = Backbone.View.extend({
+//handles message function
+var SubmitView = Backbone.View.extend({
 
   events: {
     'submit' : 'handleSubmit'
   },
 
-  initialize: function(){
-
-  },
-
   handleSubmit: function(){
-
+    //emits message to server via socket.io
+    socket.emit('chat message', $('#chatInput').val());
+    //reset input field
+    $('#chatInput').val('');
+    return false;
   }
 
 });
 
-var messageView = Backbone.View.extend({
-  template : _.template('<div><%- username %></div>\
-                         <div> <%- text %></div>\
-                         <div><%= lang %></div>'),
+var MessageView = Backbone.View.extend({
+  template : _.template('<div> <%- text %></div>'),
 
   render:function(){
     this.$el.html(this.template(this.model.attributes));
-    return this;
+    return this.$el;
   }
 });
 
-var messagesView = Backbone.View.extend({
-  initialize : function(){
-    this.collection.on('sync', this.render, this);
-    // this.collection.on('change:translate', this.render, this)
-  },
-  render : function(){
+var MessagesView = Backbone.View.extend({
 
+  initialize : function(){
+    var collect = this.collection;
+    this.collection.on('add', this.render, this);
+
+    //socket.io listener for emits
+    socket.on('chat message', function(msg){
+      //adds message to collection
+      collect.addmsg(msg);
+    });
+
+    //storage variable for
+    this.onscreenMessages = {};
+  },
+
+  render : function () {
+    this.collection.forEach(this.renderMessage, this);
+  },
+
+  renderMessage : function(message) {
+    //message.cid is unique client-only id
+    if (!this.onscreenMessages[message.cid]) {
+      var messageView = new MessageView ({model : message});
+      this.$el.prepend (messageView.render());
+      this.onscreenMessages[message.cid] = true;
+    }
   }
 
+});
+
+//initialize
+$(function(){
+  var messages = new Messages();
+  var submitView = new SubmitView({el: $('#submitView'), collection: messages});
+  var messagesView = new MessagesView({el: $('#messagesView'), collection: messages});
 });
