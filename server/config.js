a96f561d9879e63cfa47adebba03c771ed1912f9
Dave Fedele <davefedele@users.noreply.github.com>
2015-01-26 16:33:01 -0800

    Delete config.js

--- server/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  accessToken : "Bearer Audience=https%3a%2f%2fapi.datamarket.azure.com%2f&ExpiresOn=1422053049&Issuer=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2005%2f05%2fidentity%2fclaims%2fnameidentifier=459ac684-fb1c-4992-a6b1-8a6d2defc609&http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2009%2f09%2fidentity%2fclaims%2factor=DataRP&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fpermissions=account&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fidentityprovider=DataMarketIdentityProvider&HMACSHA256=JyFDUSC6%2bZQFiNcysQRmC6Bgh7ucFhYhAa8LCC%2fXZlQ%3d"
-}
-
-module.exports = config;
